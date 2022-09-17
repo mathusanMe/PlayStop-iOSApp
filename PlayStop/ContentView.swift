10b49f1a85dbf5756f1ad09731a2aef09e861ef6
mathusan selvakumar <mathusan.selvakumar@gmail.com>
2022-09-17 13:56:23 +0200

    Initial Commit

---
+++ PlayStop/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PlayStop
+//
+//  Created by mathusan selvakumar on 17/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PlayStop/PlayStopApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayStopApp.swift
+//  PlayStop
+//
+//  Created by mathusan selvakumar on 17/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct PlayStopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
