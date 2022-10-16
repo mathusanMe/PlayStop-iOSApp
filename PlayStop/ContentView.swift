d9abec4db1c02803be80da4b8499adaf2d8b4aac
mathusan selvakumar <mathusan.selvakumar@gmail.com>
2022-10-16 13:10:12 +0200

    Currently having a bug with exception raised!

--- PlayStop/ContentView.swift
+++ PlayStop/ContentView.swift
@@ -8,29 +8,50 @@
 import SwiftUI
 import IGDB_SWIFT_API
 
-struct Game: Identifiable {
+struct prevGame: Identifiable {
     let id: UInt64
     var name: String
 }
 
+struct Game: Identifiable {
+    let id: UInt64
+    var name: String
+    var coverUrl: String
+    
+}
+
 struct ContentView: View {
-    @State var games: [Game] = []
-    @State var text: String = ""
+    @State private var games: [Game] = []
+    @State private var prevGames: [prevGame] = []
+    @State private var searchText: String = ""
     
     var body: some View {
-        VStack {
-            TextField("Enter the name", text: $text).onChange(of: text) { newValue in
-                testSearch(query: text)
+        NavigationView {
+            List(games) { game in
+                HStack {
+                    AsyncImage(url: URL(string: game.coverUrl)) { image in
+                        image.resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(maxWidth: 100)
+                    } placeholder: {
+                        ProgressView()
+                    }
+
+                    Text(game.name)
+                }
             }
-            ForEach(games) { game in
-                Text(game.name)
+            .listStyle(.plain)
+            .searchable(text: $searchText)
+            .onChange(of: searchText) { value in
+                Task {
+                    await testSearch(value: value)
+                }
             }
         }
+        .navigationTitle("Games")
         .onAppear {
-            
             IGDBClient.postAuthentification(completion: handlePostAuthentification(success:error:))
         }
-            
     }
     
     func handlePostAuthentification(success: Bool, error: Error?) {
@@ -40,19 +61,38 @@ struct ContentView: View {
         }
     }
     
-    func testSearch(query: String) {
-        let query = APICalypse()
-            .search(searchQuery: query)
-            .fields(fields: "game.*")
-            .where(query: "game != n & game.version_parent = n")
-
-        IGDBClient.wrapper.search(apiCalypse: query, result: { search in
-            games = []
-            for singleSearch in search {
-                games.append(Game(id: singleSearch.id, name: singleSearch.game.name))
-            }
-        }) { error in
+    func testSearch(value: String) async {
+        if !value.isEmpty && value.count > 3 {
+            let query = APICalypse()
+                .search(searchQuery: value)
+                .fields(fields: "game.*")
+                .where(query: "game != n & game.version_parent = n")
             
+            IGDBClient.wrapper.search(apiCalypse: query, result: { search in
+                for singleSearch in search {
+                    prevGames.append(prevGame(id: singleSearch.game.id, name: singleSearch.game.name))
+                }
+            }) { error in
+                
+            }
+            
+            for game in prevGames {
+                let query = APICalypse()
+                    .fields(fields: "image_id")
+                    .where(query: "image_id != n & game.version_parent = n & game = \(game.id)")
+                print(game.name)
+                IGDBClient.wrapper.covers(apiCalypse: query, result: { covers in
+                    if (covers.count > 0) {
+                        games.append(Game(id: game.id, name: game.name, coverUrl: imageBuilder(imageID: covers[0].imageID, size: .COVER_SMALL)))
+                    }
+                }) { error in
+
+                }
+            }
+        }
+        else {
+            prevGames.removeAll()
+            games.removeAll()
         }
     }
 }
